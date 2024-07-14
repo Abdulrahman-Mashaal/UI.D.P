c30fc49f07b603c461879a32140a386791b10b1b
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2024-07-14 14:04:47 +0300

    preparation steps

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: () => import("@/views/Home.vue"),
+      },
+  
+  {
+    path: "/:catchAll(.*)",
+    name: "404*",
+    component: import(/* webpackChunkName: "error404" */ "@/views/404.vue"),
+  },
+];
+
+const router = createRouter({
+  linkActiveClass: "active",
+  scrollBehavior() {
+    return { top: 0, behavior: "smooth" };
+  },
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
