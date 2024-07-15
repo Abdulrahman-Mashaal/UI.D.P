b3ac5982cf8027e5cc0a8ff7a5e7ad3852002935
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2024-07-15 20:57:46 +0300

    setup

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
             name: "Home",
             component: () => import("@/views/Home.vue"),
           },
+          {
+            path: "link",
+            name: "Link",
+            component: () => import("@/views/Link.vue"),
+          },
       ]
       },
   
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from 'node:url'
-// import path, { resolve, dirname } from 'path'
-// const require = createRequire( import.meta.url );
-import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve:{
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@images': fileURLToPath(new URL('./src/assets/images', import.meta.url)),
     }
   },
   server: {
