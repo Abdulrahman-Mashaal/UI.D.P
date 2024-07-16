8ecec4e87aa3e3f09d3f733640afa6491732b255
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2024-07-16 14:05:10 +0300

    about section

---
+++ src/views/About.vue
@@ -0,0 +1,47 @@
+<script setup>
+import aboutSales from '@images/vector/about-sales.png'
+import aboutMarketing from '@images/vector/about-marketing.png'
+import aboutSupport from '@images/vector/about-support.png'
+</script>
+<template>
+    <section class="py-20 px-24">
+        <div class="flex items-center justify-between mb-12">
+            <h3>About us</h3>
+            <div class="flex items-center gap-4">
+             <button class="w-8 h-8 rounded-full border border-[#5B5B5B] justify-center before-obj gap-2 before:content-[url('@images/icons/left-arrow-gray.svg')]"></button>   
+             <button class="w-8 h-8 rounded-full border border-[#5B5B5B] justify-center before-obj gap-2 before:content-[url('@images/icons/right-arrow-gray.svg')]"></button>   
+            </div>
+        </div>
+        <div class="overflow-hidden flex items-stretch gap-6">
+            <div class="p-4 pb-8 bg-[#F7F8FA] rounded-xl max-w-[400px] border border-[#EAEBEF]">
+                <div class="mb-6 h-9/12">
+                    <img :src="aboutSales" alt="about sales" class="w-full h-auto">
+                </div>
+                <div>
+                    <h5>Sales</h5>
+                    <p class="text-sm text-[#ABACB2] ml-1 mr-11">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt non, perspiciatis dolor ab repellendus et!</p>
+                </div>
+            </div>
+            <div class="p-4 pb-8 bg-[#F7F8FA] rounded-xl max-w-[400px] border border-[#EAEBEF]">
+                <div class="mb-6 h-9/12">
+                    <img :src="aboutMarketing" alt="about marketing" class="w-full h-auto">
+                </div>
+                <div>
+                    <h5>Marketing</h5>
+                    <p class="text-sm text-[#ABACB2] ml-1 mr-11">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt non, perspiciatis dolor ab repellendus et!</p>
+                </div>
+            </div>
+            <div class="p-4 pb-8 bg-[#F7F8FA] rounded-xl max-w-[400px] border border-[#EAEBEF]">
+                <div class="mb-6 h-9/12">
+                    <img :src="aboutSupport" alt="about support" class="w-full h-auto">
+                </div>
+                <div>
+                    <h5>Support</h5>
+                    <p class="text-sm text-[#ABACB2] ml-1 mr-11">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt non, perspiciatis dolor ab repellendus et!</p>
+                </div>
+            </div>
+        </div>
+      </section>
+</template>
+<style scoped>
+</style>
---
+++ src/views/Identity.vue
@@ -0,0 +1,20 @@
+<script setup>
+import identityImg from '@images/vector/identity.png'
+</script>
+<template>
+    <section class="flex items-center justify-between py-20 px-24 bg-[url('@images/background/identity.png')] bg-cover bg-center">
+        <div class="flex flex-col items-start justify-center max-w-[500px]">
+            <h1 class="text-white-700 mb-6">Start with your identity</h1>
+            <p class="mb-10 text-white-700 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi a consequatur voluptate voluptas officia minima illo dicta</p>
+            <div class="flex items-center gap-6">
+                <button class="btn-default btn-reversed-flat rounded-3xl">Free trial</button>
+                <button class="btn-default btn-outlined rounded-3xl">Talk to us</button>
+            </div>
+        </div>
+        <div class="max-w-[587px]">
+            <img :src="identityImg" alt="identity" class="w-full h-auto">
+        </div>
+     </section>
+</template>
+<style scoped>
+</style>
