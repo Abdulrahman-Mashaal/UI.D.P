31dcd5549385b61ca4f372a4772f7c03c0615f04
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2024-07-16 17:16:01 +0300

    Solution section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,14 @@
 import Identity from '@/views/Identity.vue';
 import About from '@/views/About.vue';
 import Automation from '@/views/Automation.vue';
+import Solution from '@/views/Solution.vue';
 </script>
 <template>
     <div>
          <Identity/>
          <About/>
          <Automation/>
+         <Solution/>
     </div>
 </template>
 <style scoped>
