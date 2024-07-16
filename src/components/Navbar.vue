139ffa738436f3c8d49e3d08d3d0b62f06c4d400
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2024-07-16 09:55:56 +0300

    Set transition, before, and after utility classes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,7 +10,7 @@ import logo from '@images/logo.svg';
     </div>
       <div class="flex items-center gap-10">
         <button
-          class="nav-link relative inline-flex items-center gap-1 after:inline-flex after:items-center after:justify-center after:w-5 after:h-5 after:leading-[0] after:content-[url('@images/down-arrow.svg')] after:transition-rotate after:duration-300 hover:after:-rotate-180"
+          class="nav-link after-obj gap-1 after:content-[url('@images/down-arrow.svg')] hover:after:-rotate-180"
         >
           Cards
         </button>
@@ -19,7 +19,7 @@ import logo from '@images/logo.svg';
       </div>
       <div class="flex items-center gap-10">
         <router-link to="/link">Login</router-link>
-        <button class="btn-compact btn-flat rounded-3xl">Apply now</button>
+        <button class="btn-compact btn-flat rounded-3xl after-obj gap-2 after:content-[url('@images/right-arrow.svg')] hover:after:translate-x-1">Apply now</button>
       </div>
   </nav>
 </template>
