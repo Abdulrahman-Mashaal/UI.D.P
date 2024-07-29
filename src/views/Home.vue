620b819b8981259a31a76c253ca6557c2ac1719a
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2024-07-29 10:39:09 +0300

    finalize plan section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,14 @@ import Hero from '@/views/Hero.vue'
 import Features from '@/views/Features.vue'
 import Excellence from '@/views/Excellence.vue'
 import Integration from '@/views/Integration.vue'
+import Plan from '@/views/Plan.vue'
 </script>
 <template>
     <Hero/>
     <Features/>
     <Excellence/>
     <Integration/>
+    <Plan/>
 
 </template>
 <style scoped></style>
---
+++ src/views/Plan.vue
@@ -0,0 +1,70 @@
+<script setup>
+</script>
+<template>
+    <section class="bg-wildSand-50 pt-[152px] pb-[145px]">
+        <div class="container ">
+            <div class="max-w-[986px] mx-auto">
+                <div class="flex flex-col items-center justify-center">
+                    <p class="w-fit rounded-[100px] px-4 py-[3px] text-sm bg-white border border-mercury-200">Packages</p>
+                    <h2 class="mt-1 capitalize">plan of pricing</h2>
+                </div>
+                <div class="mt-[22px] grid grid-cols-2 gap-[26px]">
+                    <!-- card -->
+                    <div class="p-[30px] bg-white rounded border border-mercury-200 flex flex-col ">
+                        <div class="flex items-center justify-between pb-5 mb-[29px] border-b border-mercury-200">
+                            <p class="text-2xl text-black">standard</p>
+                            <p class="text-2xl text-black">$ 29.99 USD<span>mo</span></p>
+                        </div>
+                        <p class="mb-[31px] text-sm text-mineShaft-900">Unlock the full potential of your business with our Standard Plan
+                            —a feature-packed solution designed to elevate your workflow
+                            &amp; collaboration.</p>
+                            <button class="btn btn--lg btn--outlined after-obj font-bold gap-3 after:content-[url('@images/icons/right-arrow.svg')] mb-[30px]">get started</button>
+                            <ul class="flex flex-col spacer-y-4">
+                                <li class="before-obj gap-[10px] before:content-[url('@images/icons/check-mark.svg')] text-sm text-mineShaft-900 border-b border-mercury-200 pb-[17.5px]">
+                                <span>20GB Storage capacity</span>
+                                </li>
+                                <li class="before-obj gap-[10px] before:content-[url('@images/icons/check-mark.svg')] text-sm text-mineShaft-900 border-b border-mercury-200 pb-[17.5px]">
+                                <span>Up to 10 users limits</span>
+                                </li>
+                                <li class="before-obj gap-[10px] before:content-[url('@images/icons/check-mark.svg')] text-sm text-mineShaft-900 border-b border-mercury-200 pb-[17.5px]">
+                                <span>Extensive business guides</span>
+                                </li>
+                                <li class="before-obj gap-[10px] before:content-[url('@images/icons/check-mark.svg')] text-sm text-mineShaft-900 border-b border-mercury-200 pb-[17.5px]">
+                                <span>Priority customer support</span>
+                                </li>
+                            </ul>
+                            <p class="mt-[30px] text-sm text-mineShaft-900 text-center">Flexibility: Pause or cancel anytime</p>
+                    </div>
+                    <!-- card -->
+                    <div class="p-[30px] bg-white rounded border border-mercury-200 flex flex-col ">
+                        <div class="flex items-center justify-between pb-5 mb-[29px] border-b border-mercury-200">
+                            <p class="text-2xl text-black">standard</p>
+                            <p class="text-2xl text-black">$ 29.99 USD<span>mo</span></p>
+                        </div>
+                        <p class="mb-[31px] text-sm text-mineShaft-900">Unlock the full potential of your business with our Standard Plan
+                            —a feature-packed solution designed to elevate your workflow
+                            &amp; collaboration.</p>
+                            <button class="btn btn--lg btn--outlined after-obj font-bold gap-3 after:content-[url('@images/icons/right-arrow.svg')] mb-[30px]">get started</button>
+                            <ul class="flex flex-col spacer-y-4">
+                                <li class="before-obj gap-[10px] before:content-[url('@images/icons/check-mark.svg')] text-sm text-mineShaft-900 border-b border-mercury-200 pb-[17.5px]">
+                                <span>20GB Storage capacity</span>
+                                </li>
+                                <li class="before-obj gap-[10px] before:content-[url('@images/icons/check-mark.svg')] text-sm text-mineShaft-900 border-b border-mercury-200 pb-[17.5px]">
+                                <span>Up to 10 users limits</span>
+                                </li>
+                                <li class="before-obj gap-[10px] before:content-[url('@images/icons/check-mark.svg')] text-sm text-mineShaft-900 border-b border-mercury-200 pb-[17.5px]">
+                                <span>Extensive business guides</span>
+                                </li>
+                                <li class="before-obj gap-[10px] before:content-[url('@images/icons/check-mark.svg')] text-sm text-mineShaft-900 border-b border-mercury-200 pb-[17.5px]">
+                                <span>Priority customer support</span>
+                                </li>
+                            </ul>
+                            <p class="mt-[30px] text-sm text-mineShaft-900 text-center">Flexibility: Pause or cancel anytime</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped>
+</style>
