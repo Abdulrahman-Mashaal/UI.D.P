ff9b3783bf3f5f61aaaf0dde0e3c81447c3d1ce3
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2024-07-29 09:02:14 +0300

    finalize futures section

--- src/views/Features.vue
+++ src/views/Features.vue
@@ -22,6 +22,43 @@
                     <img src="@images/vector/feature02.png" alt="feature two" class="mx-auto mt-[70px]">
                 </div>
             </div>
+
+            <div class="grid grid-cols-2 gap-24 mt-[130px]">
+                <aside class="flex flex-col justify-center">
+                    <h3 class="mb-[14px]">Smarter management for your business</h3>
+                    <p class="mb-[26px]">Our innovative solutions provide a clear direction, helping you streamline workflows, organize tasks effortlessly, &amp; achieve unparalleled efficiency. Let Zenify Navigator be your compass on the journey to intelligent &amp; effective business management.</p>
+                    <ul class="flex flex-col spacer-y-4 mb-[42px]">
+                        <li class="before-obj gap-[10px] before:content-[url('@images/icons/checked-icon.svg')]">
+                        <span>Intuitive Task Organization</span>
+                        </li>
+                        <li class="before-obj gap-[10px] before:content-[url('@images/icons/checked-icon.svg')]">
+                        <span>Customizable Dashboards</span>
+                        </li>
+                        <li class="before-obj gap-[10px] before:content-[url('@images/icons/checked-icon.svg')]">
+                        <span>User Training and Support</span>
+                        </li>
+                    </ul>
+                    <button class="btn btn--lg btn--outlined w-fit px-6 after-obj gap-3 after:content-[url('@images/icons/right-arrow.svg')]">explore more</button>
+                </aside>
+                <img src="@images/vector/feature03.png" alt="feature three">
+            </div>
+            
+            <div class="grid grid-cols-2 gap-24 mt-[130px]">
+                <img src="@images/vector/feature04.png" alt="feature four">
+                <aside class="flex flex-col justify-center">
+                    <h3 class="mb-[14px]">Innovation &amp; dynamic solutions to empower</h3>
+                    <p class="mb-5">Zenify Empower is dedicated to revolutionizing your business by empowering it
+                        through innovation and dynamic solutions. Our commitment to cutting-edge
+                        technology ensures that your organization stays ahead in a rapidly evolving
+                        landscape.</p>
+                        <p>
+                            With a focus on forward-thinking solutions, we empower your team to embrace
+change, foster creativity, and drive unprecedented success.
+                        </p>
+                   
+                    <button class="btn btn--lg btn--outlined w-fit px-6 after-obj gap-3 after:content-[url('@images/icons/right-arrow.svg')] mt-[42px]">discover now</button>
+                </aside>
+            </div>
         </div>
     </section>
 </template>
