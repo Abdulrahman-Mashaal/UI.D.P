1b9ecf819fd5bf20459c880c12918caae2fbefb7
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2024-07-30 14:43:04 +0300

    set base of colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,100 +31,63 @@ module.exports = {
       "2xl": "1400px",
     },
     fontFamily: {
-      helvetica: "Helvetica, sans-serif",
-      aeonik: "Aeonik, sans-serif",
+      Inter: "Inter, sans-serif",
     },
     fontSize: {
       sm: [
         ".875rem",
         {
-          lineHeight: "1.25",
+          lineHeight: "1.5", // font-size/line-height
           fontWeight: "400",
         },
       ],
       base: [
-        "1.125rem",
+        "1rem",
         {
-          lineHeight: "1.25",
+          lineHeight: "1.68",
           fontWeight: "400",
         },
       ],
       lg: [
-        "1.25rem",
+        "1.125rem",
         {
           lineHeight: "1.5",
-          fontWeight: "500",
+          fontWeight: "400",
         },
       ],
       xl: [
-        "1.625rem",
+        "1.25rem",
         {
-          lineHeight: "1.26",
-          fontWeight: "500",
+          lineHeight: "0.7",
+          fontWeight: "600",
         },
       ],
       "2xl": [
-        "2rem",
+        "1.625rem",
         {
           lineHeight: "1.5",
           fontWeight: "600",
         },
       ],
       "3xl": [
-        "3.125rem",
+        "2.125rem",
         {
-          lineHeight: "1.1",
+          lineHeight: "1.5",
           fontWeight: "700",
         },
       ],
       "4xl": [
-        "3.75rem",
+        "3.25rem",
         {
-          lineHeight: "1.1",
+          lineHeight: "1.19",
           fontWeight: "700",
         },
       ],
     },
     colors: {
-      primary: {
-        50: "#ecfffd",
-        100: "#cffefb",
-        200: "#a5fcf9",
-        300: "#67f9f6",
-        400: "#22eeed",
-        500: "#06d3d4",
-        600: "#08a9b2",
-        700: "#0e8690",
-        800: "#16707a",
-        900: "#165963",
-        950: "#083b44",
-      },
-      black: {
-        700: "#000000",
-      },
-      white: {
-        700: "#ffffff",
-        600: "#FAFAFA",
-      },
-      green: {
-        700: "#B8E1D680",
-        600: "#00A69D",
-        500: "#B8E1D6",
-      },
-      blue: {
-        700: "#09273B",
-      },
-      gray: {
-        700: "#4C5768",
-        600: "#7E8299",
-        500: "#666666",
-        400: "#D9D9D9",
-        300: "#858585",
-        200: "#C7C7C7",
-        100: "#C8C8C8",
-      },
-      hero: "rgb(184, 225, 214)",
       transparent: "transparent",
+      black: '#000',
+      white: '#ffffff',
     },
   },
 
