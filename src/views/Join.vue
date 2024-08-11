4d23776de61fa3aae8a603c580386681a7a4e408
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2024-08-11 12:37:58 +0300

    join section

--- src/views/Join.vue
+++ src/views/Join.vue
@@ -1,13 +1,59 @@
 <script setup>
-import checkedSVG from '@images/icons/checked.svg'
+import youtubeSVG from '@images/icons/youtube.svg?component'
+import publishSVG from '@images/icons/publish.svg?component'
+import usersSVG from '@images/icons/users.svg?component'
 </script>
 <template>
     <section class="py-5 lg:py-[120px] pt-9 pb-8 bg-white">
         <div class="container">
             <div class="max-w-[1300px] 3xl:max-w-[1517px] flex flex-col items-center justify-center text-center mx-auto px-[1px]">
-                <h2 class="text-xl mb-5 lg:mb-0 lg:text-4xl font-Hind font-normal">What You Get Access When You Join Aquarius</h2>
-                <p class="mb-[70px] text-sm lg:text-lg font-Hind font-normal">Discover the Glowing Testimonials That Tell Our Story</p>
-                
+                <h2 class="text-xl mb-5 lg:mb-0 lg:text-4xl font-Hind font-normal">YouTube Content Creation</h2>
+                <p class="mb-[70px] text-sm lg:text-lg font-Hind font-normal">Figure out how to use YouTube to attract people who might be interested</p>
+            </div>
+            <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-y-[18px] gap-x-7">
+                <div class="py-7 px-8 border-2 border-[#EAE8E8] rounded-xl">
+                    <youtubeSVG alt="youtube icon" class="mb-5 mx-auto"/>
+                    <div class=" max-w-[392px] mx-auto">
+                        <p class="text-center text-plantation-800 text-base font-medium font-Hind mb-[14px]">Teach people how to Make Videos and offer to edit them.</p>
+                        <p class="line-clamp-2 text-center text[#617975] text-sm font-normal">Figure out how to use YouTube to attract people who might be interested ...</p>
+                    </div>
+                </div>
+                <div class="py-7 px-8 border-2 border-[#EAE8E8] rounded-xl">
+                    <usersSVG alt="youtube icon" class="mb-5 mx-auto"/>
+                    <div class=" max-w-[392px] mx-auto">
+                        <p class="text-center text-plantation-800 text-base font-medium font-Hind mb-[14px]">Teach people how to Make Videos and offer to edit them.</p>
+                        <p class="line-clamp-2 text-center text[#617975] text-sm font-normal">Figure out how to use YouTube to attract people who might be interested ...</p>
+                    </div>
+                </div>
+                <div class="py-7 px-8 border-2 border-[#EAE8E8] rounded-xl">
+                    <publishSVG alt="youtube icon" class="mb-5 mx-auto"/>
+                    <div class=" max-w-[392px] mx-auto">
+                        <p class="text-center text-plantation-800 text-base font-medium font-Hind mb-[14px]">Teach people how to Make Videos and offer to edit them.</p>
+                        <p class="line-clamp-2 text-center text[#617975] text-sm font-normal">Figure out how to use YouTube to attract people who might be interested ...</p>
+                    </div>
+                </div>
+                <div class="py-7 px-8 border-2 border-[#EAE8E8] rounded-xl">
+                    <youtubeSVG alt="youtube icon" class="mb-5 mx-auto"/>
+                    <div class=" max-w-[392px] mx-auto">
+                        <p class="text-center text-plantation-800 text-base font-medium font-Hind mb-[14px]">Teach people how to Make Videos and offer to edit them.</p>
+                        <p class="line-clamp-2 text-center text[#617975] text-sm font-normal">Figure out how to use YouTube to attract people who might be interested ...</p>
+                    </div>
+                </div>
+                <div class="py-7 px-8 border-2 border-[#EAE8E8] rounded-xl">
+                    <publishSVG alt="youtube icon" class="mb-5 mx-auto"/>
+                    <div class=" max-w-[392px] mx-auto">
+                        <p class="text-center text-plantation-800 text-base font-medium font-Hind mb-[14px]">Teach people how to Make Videos and offer to edit them.</p>
+                        <p class="line-clamp-2 text-center text[#617975] text-sm font-normal">Figure out how to use YouTube to attract people who might be interested ...</p>
+                    </div>
+                </div>
+                <div class="py-7 px-8 border-2 border-[#EAE8E8] rounded-xl">
+                    <usersSVG alt="youtube icon" class="mb-5 mx-auto"/>
+                    <div class=" max-w-[392px] mx-auto">
+                        <p class="text-center text-plantation-800 text-base font-medium font-Hind mb-[14px]">Teach people how to Make Videos and offer to edit them.</p>
+                        <p class="line-clamp-2 text-center text[#617975] text-sm font-normal">Figure out how to use YouTube to attract people who might be interested ...</p>
+                    </div>
+                </div>
+               
             </div>
         </div>
     </section>
