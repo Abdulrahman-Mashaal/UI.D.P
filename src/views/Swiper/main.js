fba22a09f54c6d3a0155303f17058cd150ab9f6b
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2025-02-13 13:40:01 +0200

    futureface clients slider

--- src/views/Swiper/main.js
+++ src/views/Swiper/main.js
@@ -71,3 +71,28 @@ const futurefaceNewsSlider = new Swiper('.futureface-news', {
     },
 
 });
+const futurefaceOurClientsSlider = new Swiper('.futureface-our-clients', {
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 0,
+    speed: 5000,
+    allowTouchMove: false,
+    autoplay: {
+        delay: 1,
+        disableOnInteraction: false
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 24
+        },
+        1024: {
+            slidesPerView: 5,
+            spaceBetween: 32
+        }
+    }
+});
