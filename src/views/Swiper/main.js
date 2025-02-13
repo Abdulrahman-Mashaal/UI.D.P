d5d4fcf4db30c55898e75071cda255412bde116d
Abdulrahman Mashaal <abdul.attiamashaal@gmail.com>
2025-02-13 22:08:33 +0200

    robox usage swiper

--- src/views/Swiper/main.js
+++ src/views/Swiper/main.js
@@ -135,3 +135,63 @@ const futurefaceOurClientsSlider = new Swiper('.futureface-our-clients', {
         }
     }
 });
+
+
+// Initialize Swiper for tabs
+const roboxUsageTabSlider = new Swiper(".usage-tabs", {
+    slidesPerView: "auto",
+    spaceBetween: 15,
+    preventClicks: true,
+    preventClicksPropagation: false,
+    observer: true,
+    observeParents: true
+  });
+  
+  // Click event for tab items
+  const snbSwiperItems = document.querySelectorAll(".usage-tabs .swiper-wrapper .swiper-slide a");
+  snbSwiperItems.forEach(item => {
+    item.addEventListener('click', (event) => {
+      event.preventDefault();
+      snbSwiperItems.forEach(el => el.parentElement.classList.remove("on"));
+      const target = item.parentElement;
+      target.classList.add("on");
+      muCenter(target);
+    });
+  });
+  
+  const muCenter = (target) => {
+    const snbwrap = document.querySelector(".usage-tabs .swiper-wrapper");
+    const box = document.querySelector(".usage-tabs");
+    const boxHalf = box.clientWidth / 2;
+    const targetPos = target.offsetLeft + target.clientWidth / 2;
+    const pos = targetPos - boxHalf;
+  
+    // Ensure pos is within valid bounds
+    const minPos = 0;
+    const maxPos = snbwrap.scrollWidth - box.clientWidth;
+  
+    snbwrap.style.transition = "transform 0.25s";
+    snbwrap.style.transform = `translate3d(${Math.min(Math.max(pos, minPos), maxPos) * -1}px, 0, 0)`;
+  };
+  
+  // Initialize Swiper for content
+  const roboxUsageSlider = new Swiper(".usage-content", {
+    slidesPerView: 1,
+    spaceBetween: 32,
+    allowTouchMove: true,
+    autoplay: {
+        delay: 2000
+    },
+    thumbs: {
+      swiper: roboxUsageTabSlider
+    }, 
+    on: {
+      slideChange: function () {
+        const target = this.activeIndex;
+        snbSwiperItems.forEach(el => el.parentElement.classList.remove("on"));
+        snbSwiperItems[target].parentElement.classList.add("on");
+        muCenter(snbSwiperItems[target].parentElement);
+      }
+    }
+  });
+  
